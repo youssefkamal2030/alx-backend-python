2ebf9431a8cd73a2aaa0e6b3d3949a501c3c7c34
youssef <youssefkamal856@yahoo.com>
2024-10-17 13:47:18 +0300

    done

--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-concat = __import__('1-concat').concat
 
-str1 = "egg"
-str2 = "shell"
+a = __import__('4-define_variables').a
+pi = __import__('4-define_variables').pi
+i_understand_annotations = __import__('4-define_variables').i_understand_annotations
+school = __import__('4-define_variables').school
 
-print(concat(str1, str2) == "{}{}".format(str1, str2))
-print(concat.__annotations__)
+print("a is a {} with a value of {}".format(type(a), a))
+print("pi is a {} with a value of {}".format(type(pi), pi))
+print("i_understand_annotations is a {} with a value of {}".format(type(i_understand_annotations), i_understand_annotations))
+print("school is a {} with a value of {}".format(type(school), school))
